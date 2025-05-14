6124c4d3a567e63899ad15f15e3375c352ef99a4
filiplarsen231 <filiplarsen231@gmail.com>
2025-05-14 12:58:22 +0200

    massor

--- lib/pages/history_view.dart
+++ lib/pages/history_view.dart
@@ -3,7 +3,10 @@ import 'package:api_test/model/imat/order.dart';
 import 'package:api_test/model/imat/product.dart';
 import 'package:api_test/model/imat/shopping_item.dart';
 import 'package:api_test/model/imat_data_handler.dart';
+import 'package:api_test/pages/account_view.dart';
+import 'package:api_test/pages/main_view.dart';
 import 'package:api_test/widgets/buy_button.dart';
+import 'package:api_test/widgets/cart_view.dart';
 import 'package:api_test/widgets/buy_order_button.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -21,6 +24,7 @@ class HistoryView extends StatefulWidget {
 
 class _HistoryViewState extends State<HistoryView> {
   Order? _selectedOrder;
+  bool _isHovered = false;
 
   @override
   Widget build(BuildContext context) {
@@ -35,8 +39,24 @@ class _HistoryViewState extends State<HistoryView> {
       body: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          SizedBox(height: AppTheme.paddingLarge),
-          _header(context),
+          Container(
+            color: AppTheme.customPanelColor,
+
+            child: Column(
+              children: [
+                SizedBox(height: AppTheme.paddingSmall),
+                _header(context),
+                SizedBox(height: AppTheme.paddingSmall),
+                ColoredBox(
+                  color: Colors.black,
+                  child: SizedBox(
+                    height: 2,
+                    width: MediaQuery.of(context).size.width,
+                  ),
+                ),
+              ],
+            ),
+          ),
           Expanded(
             child: Row(
               children: [
@@ -46,14 +66,31 @@ class _HistoryViewState extends State<HistoryView> {
                   // Creates the list to the left.
                   // When a user taps on an item the function _selectOrder is called
                   // The Material widget is need to make hovering pliancy effects visible
-                  child: Material(
-                    color: AppTheme.customPanelColor,
-                    child: _ordersList(context, orders, _selectOrder),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 1,
+                          blurRadius: 5,
+                          offset: Offset(0, 2), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: Material(
+                      color: Colors.white,
+                      child: _ordersList(context, orders, _selectOrder),
+                    ),
                   ),
                 ),
                 // Creates the view to the right showing the
                 // currently selected order.
                 Expanded(child: _orderDetails(_selectedOrder)),
+                Container(
+                  width: 300,
+                  //color: Colors.blueGrey,
+                  child: _shoppingCart(iMat),
+                ),
               ],
             ),
           ),
@@ -62,23 +99,119 @@ class _HistoryViewState extends State<HistoryView> {
     );
   }
 
-  Widget _header(BuildContext context) {
+  Widget _shoppingCart(ImatDataHandler iMat) {
+    return Container(
+      width: 300,
+      constraints: BoxConstraints(
+        minHeight:
+            MediaQuery.of(context).size.height - AppTheme.paddingMedium * 2,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 1,
+            blurRadius: 5,
+            offset: Offset(-2, 0), // changes position of shadow
+          ),
+        ],
+      ),
+      padding: EdgeInsets.all(AppTheme.paddingMedium),
+      child: Column(
+        children: [
+          Text('Kundvagn'),
+          Expanded(child: CartView()),
+          ElevatedButton(
+            onPressed: () {
+              iMat.placeOrder();
+            },
+            child: Text('Köp!'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container _header(BuildContext context) {
     return Container(
       color: AppTheme.customPanelColor,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text('iMat'),
+          Padding(
+            padding: const EdgeInsets.only(left: 30),
+            child: MouseRegion(
+              cursor: SystemMouseCursors.click,
+              onEnter: (_) => setState(() => _isHovered = true),
+              onExit: (_) => setState(() => _isHovered = false),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => MainView()),
+                  );
+                },
+
+                child: Image.asset(
+                  'assets/images/logoiMat-removebg-preview (1).png',
+                  height: 70,
+                ),
+              ),
+            ),
           ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text('Tillbaka'),
+          Container(
+            width: 700,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Sök produkter...',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+                onChanged: (value) {
+                  var iMat = Provider.of<ImatDataHandler>(
+                    context,
+                    listen: false,
+                  );
+                  if (value.isEmpty) {
+                    iMat.selectAllProducts();
+                  } else {
+                    iMat.selectSelection(iMat.findProducts(value));
+                  }
+                },
+              ),
+            ),
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HistoryView()),
+                  );
+                },
+                child: Text('Köphistorik'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AccountView()),
+                  );
+                },
+                child: Text('Användare'),
+              ),
+            ],
           ),
         ],
       ),
@@ -93,6 +226,7 @@ class _HistoryViewState extends State<HistoryView> {
 
   Widget _orderInfo(Order order, Function onTap) {
     return ListTile(
+      shape: Border(bottom: BorderSide(color: Colors.black)),
       onTap: () => onTap(order),
       title: Text('Order ${order.orderNumber}, ${_formatDateTime(order.date)}'),
     );
@@ -123,9 +257,9 @@ class _HistoryViewState extends State<HistoryView> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
-            color: Color.fromARGB(255, 228, 228, 228),
-            width: 300,
+            width: 500,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 Text(
                   'Order ${order.orderNumber}',
@@ -140,20 +274,30 @@ class _HistoryViewState extends State<HistoryView> {
                       children: [
                         SizedBox(width: 20),
                         SizedBox(
-                          width: 50,
-                          height: 50,
+                          width: 70,
+                          height: 70,
                           child: iMat.getImage(item.product),
                         ),
                         SizedBox(width: 40),
 
-                        Text('${item.product.name}, ${item.amount}'),
+                        BuyButton(
+                          onPressed: () {
+                            iMat.shoppingCartAdd(ShoppingItem(item.product));
+                          },
+                        ),
+                        SizedBox(width: 40),
+
+                        Text(
+                          '${item.product.name}, ${item.amount}',
+                          style: TextStyle(fontSize: 20),
+                        ),
                       ],
                     ),
                   ),
                 SizedBox(height: AppTheme.paddingSmall),
                 Text(
                   'Totalt: ${order.getTotal().toStringAsFixed(2)}kr',
-                  style: TextStyle(fontWeight: FontWeight.bold),
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
                 ),
                 SizedBox(height: AppTheme.paddingMedium),
                 BuyOrderButton(
