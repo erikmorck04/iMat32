221d00922afc2ed772f42fd6424525bd3ddd6ea8
filiplarsen231 <filiplarsen231@gmail.com>
2025-05-14 13:08:31 +0200

    hej

--- lib/pages/account_view.dart
+++ lib/pages/account_view.dart
@@ -8,113 +8,130 @@ import 'package:flutter/material.dart';
 
 class AccountView extends StatefulWidget {
   const AccountView({super.key});
-  
-    @override
-  State<AccountView> createState() => _MainViewState();}
 
-  class _MainViewState extends State<AccountView> {
+  @override
+  State<AccountView> createState() => _MainViewState();
+}
+
+class _MainViewState extends State<AccountView> {
   bool _isHovered = false; // State to track hover
   int _currentstep = 0;
-  
+
   void _gotonextStep() {
     setState(() {
       _currentstep = 1;
     });
-
   }
-    
+
   void _gotopreviousStep() {
     setState(() {
       _currentstep = 0;
     });
-
   }
+
   void _goToMain() {
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => MainView()));
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => MainView()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-            children: [
-              _header(context),
-              Expanded(child: 
-              Padding(
-                padding: const EdgeInsets.only(top: 60, left: 250, right: 250, bottom: 60), 
-                child: _currentstep == 0 ? _personalInfo() : _cardInfo()
+        children: [
+          _header(context),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 60,
+                left: 250,
+                right: 250,
+                bottom: 60,
               ),
-
-              )
-            ],
+              child: _currentstep == 0 ? _personalInfo() : _cardInfo(),
+            ),
           ),
+        ],
+      ),
     );
   }
 
   Widget _header(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(top: AppTheme.paddingSmall, bottom: AppTheme.paddingSmall),
+      padding: EdgeInsets.only(
+        top: AppTheme.paddingSmall,
+        bottom: AppTheme.paddingSmall,
+      ),
       color: AppTheme.customPanelColor,
-      
+
       child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 30),
-              child: MouseRegion(
-                cursor: SystemMouseCursors.click,
-                onEnter: (_) => setState(() => _isHovered = true),
-                onExit: (_) => setState(() => _isHovered = false), 
-              child:
-              GestureDetector(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 30),
+            child: MouseRegion(
+              cursor: SystemMouseCursors.click,
+              onEnter: (_) => setState(() => _isHovered = true),
+              onExit: (_) => setState(() => _isHovered = false),
+              child: GestureDetector(
                 onTap: () {
-                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => MainView()));
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => MainView()),
+                  );
                 },
-                
-                  child: Image.asset('assets/images/logoiMat-removebg-preview (1).png',height: 70,)
-                ,
+
+                child: Image.asset(
+                  'assets/images/logoiMat-removebg-preview (1).png',
+                  height: 70,
+                ),
               ),
-            )),
-        Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 30),
-              child: ElevatedButton(onPressed: _goToMain, child: Text('Tillbaka')),
-            )
-          ],
-        )
-      ]
+            ),
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 30),
+                child: ElevatedButton(
+                  onPressed: _goToMain,
+                  child: Text('Tillbaka'),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
+
   Widget _personalInfo() {
     return Container(
-      color: AppTheme.customColor,
+      color: AppTheme.customPanelColor,
       padding: const EdgeInsets.all(32),
       child: Column(
         children: [
-          Expanded(child: CustomerDetails(onSave: _gotonextStep,)),
+          Expanded(child: CustomerDetails(onSave: _gotonextStep)),
           //Align(alignment: Alignment.bottomCenter,
           //child: ElevatedButton(onPressed: _gotonextStep, child: Text('Nästa'))
           //)
-        ]
+        ],
       ),
-
     );
   }
-    Widget _cardInfo() {
+
+  Widget _cardInfo() {
     return Container(
-      color: AppTheme.customColor,
+      color: AppTheme.customPanelColor,
       padding: const EdgeInsets.all(32),
       child: Column(
         children: [
-          Expanded(child: CardDetails(onSave: _goToMain,
-            onBack: _gotopreviousStep,
-          )),
-        
-        ]
+          Expanded(
+            child: CardDetails(onSave: _goToMain, onBack: _gotopreviousStep),
+          ),
+        ],
       ),
-
     );
   }
 }
--- lib/pages/checkout_view.dart
+++ lib/pages/checkout_view.dart
@@ -9,23 +9,26 @@ class CheckoutView extends StatefulWidget {
 
 class _CheckoutView extends State<CheckoutView> {
   int _currentstep = 0;
-  
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
           _header(context),
-          Expanded(child: 
-          Padding(
-            padding: const EdgeInsets.only(top: 60, left: 250, right: 250, bottom: 60), 
-            child: _currentstep == 0 ? _personalInfo() : _cardInfo()
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 60,
+                left: 250,
+                right: 250,
+                bottom: 60,
+              ),
+              child: _currentstep == 0 ? _personalInfo() : _cardInfo(),
+            ),
           ),
-
-          )
         ],
-          ),
+      ),
     );
   }
 }
