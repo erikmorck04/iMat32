309c24d543436cf7b02d6c3e26ea11ed33f0fbcb
erikmorck04 <erikmorck04@gmail.com>
2025-05-15 15:03:19 +0200

    Update account_view.dart

--- lib/pages/account_view.dart
+++ lib/pages/account_view.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:api_test/app_theme.dart';
+import 'package:api_test/pages/history_view.dart';
 import 'package:api_test/pages/main_view.dart';
 import 'package:api_test/widgets/card_details.dart';
 import 'package:api_test/widgets/customer_details.dart';
@@ -17,6 +18,7 @@ class _AccountViewState extends State<AccountView> {
   final GlobalKey<CustomerDetailsState> customerKey = GlobalKey();
   final GlobalKey<CardDetailsState> cardKey = GlobalKey();
   int _currentstep = 0;
+  bool _isHovered = false;
 
   void _gotoNextStep() {
     setState(() {
@@ -42,7 +44,9 @@ class _AccountViewState extends State<AccountView> {
     return Scaffold(
       body: Column(
         children: [
+          SizedBox(height: AppTheme.paddingSmall),
           _header(context),
+          SizedBox(height: AppTheme.paddingSmall),
           ColoredBox(
             color: Colors.black,
             child: SizedBox(
@@ -68,32 +72,127 @@ class _AccountViewState extends State<AccountView> {
     );
   }
 
-  Widget _header(BuildContext context) {
+  Container _header(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(
-        top: AppTheme.paddingSmall,
-        bottom: AppTheme.paddingSmall,
-      ),
       color: AppTheme.customPanelColor,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 30),
-            child: GestureDetector(
-              onTap: _goToMain,
-              child: Image.asset(
-                'assets/images/logoiMat-removebg-preview (1).png',
-                height: 70,
+            Padding(
+              padding: const EdgeInsets.only(left: 30),
+              child: MouseRegion(
+                cursor: SystemMouseCursors.click,
+                onEnter: (_) => setState(() => _isHovered = true),
+                onExit: (_) => setState(() => _isHovered = false), 
+              child:
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => MainView()));
+                },
+                
+                  child: Image.asset('assets/images/logoiMat-removebg-preview (1).png',height: 70,)
+                ,
               ),
+            )
             ),
+            SizedBox(width: 32),
+          Container(width: 700,
+            child: Padding(padding: const EdgeInsets.symmetric(horizontal: 20),
+            ),),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [SizedBox(
+      height: 50,
+      child: ElevatedButton.icon(
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          backgroundColor: Colors.white,
+          elevation: 2,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+            side: BorderSide(color: AppTheme.colorScheme.primary, width: 1),
           ),
-          Padding(
-            padding: const EdgeInsets.only(right: 30),
-            child: ElevatedButton(
-              onPressed: _goToMain,
-              child: Text('Tillbaka'),
-            ),
+        ),
+        icon: Icon(Icons.home, size: 32, color: AppTheme.colorScheme.primary),
+        label: Text(
+          'Hem',
+          style: TextStyle(
+            fontSize: 22,
+            color: AppTheme.colorScheme.primary,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        onPressed: () {
+          Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => MainView()),);
+        },
+      ),
+    ),
+    SizedBox(width: 32),
+              SizedBox(
+      height: 50, // Match your search bar height
+      child: ElevatedButton.icon(
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          backgroundColor: Colors.white,
+          elevation: 2,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+            side: BorderSide(color: AppTheme.colorScheme.primary, width: 1),
+          ),
+        ),
+        icon: Icon(Icons.history, size: 32, color: AppTheme.colorScheme.primary),
+        label: Text(
+          'Köphistorik',
+          style: TextStyle(
+            fontSize: 22,
+            color: AppTheme.colorScheme.primary,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        onPressed: () {
+          
+          Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => HistoryView()),);
+        },
+      ),
+    ),
+              SizedBox(width: 32),
+    SizedBox(
+      height: 50,
+      child: ElevatedButton.icon(
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          backgroundColor: Colors.white,
+          elevation: 2,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+            side: BorderSide(color: AppTheme.colorScheme.primary, width: 1),
+          ),
+        ),
+        icon: Icon(Icons.person, size: 32, color: AppTheme.colorScheme.primary),
+        label: Text(
+          'Användare',
+          style: TextStyle(
+            fontSize: 22,
+            color: AppTheme.colorScheme.primary,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        onPressed: () {
+          Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => AccountView()),);
+        },
+      ),
+    ),
+    
+    SizedBox(width: 100)
+            ],
           ),
         ],
       ),
